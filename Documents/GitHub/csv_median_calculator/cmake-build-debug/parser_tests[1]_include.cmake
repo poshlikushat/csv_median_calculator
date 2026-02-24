e77a089e63beda5c13304b30ce409d81bad8ed54
poshlikushat <161361012+poshlikushat@users.noreply.github.com>
2026-02-24 12:25:50 +0700

    Added parser_csv with tests working correctly

--- Documents/GitHub/csv_median_calculator/cmake-build-debug/parser_tests[1]_include.cmake
+++
@@ -1,5 +0,0 @@
-if(EXISTS "/Users/kuimovmihail/Documents/GitHub/csv_median_calculator/cmake-build-debug/parser_tests[1]_tests.cmake")
-  include("/Users/kuimovmihail/Documents/GitHub/csv_median_calculator/cmake-build-debug/parser_tests[1]_tests.cmake")
-else()
-  add_test(parser_tests_NOT_BUILT parser_tests_NOT_BUILT)
-endif()
